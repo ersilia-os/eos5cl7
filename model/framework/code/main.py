1e1124313f18fab14fc08081d82a8aeec1725cd7
Richiio-portfolio <chiorlusarima@gmail.com>
2024-01-15 17:01:00 +0100

    done

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -15,23 +15,12 @@ output_file = sys.argv[2]
 
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.abspath(os.path.join(root, "..", "..", "checkpoints", "model_morgan.joblib"))
+model_path = os.path.abspath(os.path.join(root, "..", "..", "checkpoints", "model_eosce3.joblib"))
 
-# my model
-def _smi_to_fps(smiles):
-    mols = [Chem.MolFromSmiles(str(smi)) for smi in smiles]
-    fp = [AllChem.GetMorganFingerprintAsBitVect(mol, 3, nBits=2048) for mol in mols] # gets the vector
-    return fp
-
-def _load_model(model_path):
-    mdl = joblib.load(model_path)
-    return mdl
-
-def predict(smiles, model):
-    X = _smi_to_fps(smiles)
-    mdl = _load_model(model)
-    y_pred = mdl.predict_proba(X)
-    return y_pred[:,1]
+def my_model(smiles_list):
+    mdl1 = joblib.load(model_path)
+    y_pred1 = mdl1.predict_proba(smiles_list)[:,1]
+    return y_pred1
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -39,8 +28,7 @@ with open(input_file, "r") as f:
     next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
 
-# run model
-outputs = predict(smiles_list, model_path)
+outputs = my_model(smiles_list)
 
 #check input and output have the same lenght
 input_len = len(smiles_list)
